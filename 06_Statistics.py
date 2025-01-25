644ebd5d37c692ba08a430cd81133ee0d460fbc6
KGandhi90 <kevin.gandhi@somaiya.edu>
2025-01-25 22:47:02 +0530

    Statistics

---
+++ 06_Statistics.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Smallest and the Largest Element in an Array
+print(np.min(a))
+print(np.max(a))
+
+# Sum and Product of all the elements in an Array
+print(np.sum(a))
+print(np.prod(a))
+
+# Standard Deviation and Variance of all the elements in an Array
+print(np.var(a))
+print(np.std(a))
+
+# Mean and Median of all the elements in an Array
+print(np.mean(a))
+print(np.median(a))
+
+# Percentile of all the elements in an Array
+print(np.percentile(a, 99))
